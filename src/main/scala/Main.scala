707c92c9c955208572963cf08dbc1d10c2dea595
charlie <contact@charliebthomas.com>
2022-07-30 23:22:28 -0500

    MVP working

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,25 @@
+import io.lettuce.core.RedisClient
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 object Main extends App {
-  println("Hello, World!")
+
+  val client = RedisClient.create("redis://localhost/")
+  val conf = RedisSMQConfig(Left(client), "test")
+
+  val q = new RedisSMQ(conf)
+
+  val opts = CreateQueueOptions("my_queue")
+
+  Await.result(
+    for {
+      _ <- q.createQueue(opts)
+      uid <- q.sendMessage("my_queue", "Hello World")
+      foo <- q.receiveMessage("my_queue")
+    } yield uid,
+    30.seconds
+  )
+
 }
--- src/main/scala/RedisSMQ.scala
+++ src/main/scala/RedisSMQ.scala
@@ -5,6 +5,15 @@ import io.lettuce.core.{AbstractRedisClient, RedisClient, RedisFuture}
 import scala.compat.java8.FutureConverters.{CompletionStageOps, toScala}
 import scala.collection.JavaConverters._
 import scala.concurrent.{ExecutionContext, Future}
+import io.lettuce.core.api.async.RedisAsyncCommands
+import io.lettuce.core.ScriptOutputType
+import java.time.OffsetDateTime
+import io.lettuce.core
+import java.time.Instant
+import java.time.ZoneId
+import java.util.UUID
+import java.nio.ByteBuffer
+import java.util.ArrayList
 
 case class RedisSMQConfig(
     redisClient: Either[RedisClient, RedisClusterClient],
@@ -18,29 +27,221 @@ case class RedisQueue(
     ts: Long
 )
 
-class RedisSMQ(
-    config: RedisSMQConfig,
-    implicit val executionContext: ExecutionContext
-) {
+case class MessageResult(
+    id: String,
+    message: String,
+    rc: Long,
+    fr: OffsetDateTime
+)
 
-  private val redis = config.redisClient.fold(
-    redisClient => redisClient.connect().async(),
-    redisClusterClient => redisClusterClient.connect().async()
-  )
+case class ChangeMessageVisibilityOptions(qname: String, id: String, vt: Long)
+
+case class CreateQueueOptions(
+    qname: String,
+    vt: Long = 30,
+    delay: Long = 0,
+    maxsize: Long = 65536
+)
+
+case class RedisQueueAttributes(
+    qname: String,
+    vt: Long,
+    delay: Long,
+    maxsize: Long,
+    totalReceived: Long,
+    totalSent: Long,
+    created: OffsetDateTime,
+    modified: OffsetDateTime,
+    numMsgs: Long,
+    numHiddenMsgs: Long
+)
+
+class RedisSMQ(
+    config: RedisSMQConfig
+)(implicit val executionContext: ExecutionContext) {
+
+  // private val redis = config.redisClient.fold(
+  //   redisClient => redisClient.connect().async(),
+  //   redisClusterClient => redisClusterClient.connect().async()
+  // )
 
   private val ns = config.namespace
 
-  lazy val popMessageSha1 = initScript(RedisSMQScripts.PopMessage)
-  lazy val receiveMessageSha1 = initScript(RedisSMQScripts.ReceiveMessage)
-  lazy val changeMessageVisibilitySha1 = initScript(
-    RedisSMQScripts.ChangeMessageVisibility
-  )
+  def sendMessage(
+      qname: String,
+      message: String,
+      delay: Long = 0
+  ): Future[String] = {
 
-  def listQueues: Future[Set[String]] = {
-    redis.smembers(s"$ns:").map(_.asScala.toSet)
+    val key = s"{$ns:$qname}"
+    val qCommands = getRedisCommands(key)
+
+    val uid = UUID.randomUUID().toString()
+
+    for {
+      q <- this.getQueue(qname)
+      _ = qCommands.multi()
+      addFt = qCommands.zadd(key, q.ts + (delay * 1000), uid)
+      setFt = qCommands.hset(s"$key:Q", uid, message)
+      incFt = qCommands.hincrby(s"$key:Q", "totalsend", 1)
+      _ <- qCommands.exec()
+      _ <- addFt
+      _ <- setFt
+      _ <- incFt
+    } yield uid
   }
 
-  private def _getQueue(qname: String): Future[RedisQueue] = {
+  def popMessage(qname: String): Future[MessageResult] = {
+    val key = s"{$ns:$qname}"
+    val qCommands = getRedisCommands(key)
+
+    for {
+      q <- this.getQueue(qname)
+      evalResult <- qCommands
+        .eval[ArrayList[Any]](
+          RedisSMQScripts.PopMessage,
+          ScriptOutputType.MULTI,
+          key,
+          q.ts.toString
+        )
+      msg = MessageResult(
+        evalResult.get(0).asInstanceOf[String],
+        evalResult.get(1).asInstanceOf[String],
+        evalResult.get(2).asInstanceOf[Long],
+        OffsetDateTime.ofInstant(
+          Instant.ofEpochMilli(
+            evalResult.get(3).asInstanceOf[String].toLong * 1000
+          ),
+          ZoneId.systemDefault()
+        )
+      )
+    } yield msg
+  }
+
+  def receiveMessage(qname: String): Future[MessageResult] = {
+    val key = s"{$ns:$qname}"
+    val qCommands = getRedisCommands(key)
+
+    for {
+      q <- this.getQueue(qname)
+      evalResult <- qCommands
+        .eval[ArrayList[Any]](
+          RedisSMQScripts.ReceiveMessage,
+          ScriptOutputType.MULTI,
+          key,
+          q.ts.toString,
+          (q.ts + (q.vt * 1000)).toString()
+        )
+      msg = MessageResult(
+        evalResult.get(0).asInstanceOf[String],
+        evalResult.get(1).asInstanceOf[String],
+        evalResult.get(2).asInstanceOf[Long],
+        OffsetDateTime.ofInstant(
+          Instant.ofEpochMilli(
+            evalResult.get(3).asInstanceOf[String].toLong * 1000
+          ),
+          ZoneId.systemDefault()
+        )
+      )
+    } yield msg
+  }
+
+  // def listQueues: Future[Set[String]] = {
+  //   redis.smembers(s"$ns:").map(_.asScala.toSet)
+  // }
+
+  def createQueue(options: CreateQueueOptions): Future[Long] = {
+
+    val key = s"{$ns:${options.qname}}:Q"
+    val commands = getRedisCommands(key)
+    val nsCommands = getRedisCommands(s"{$ns}:QUEUES")
+
+    for {
+      time <- commands.time()
+      _ = commands.multi()
+      vtFt = commands.hsetnx(key, "vt", options.vt.toString())
+      delayFt = commands.hsetnx(key, "delay", options.delay.toString())
+      createdFt = commands.hsetnx(key, "created", time.get(0))
+      maxSizeFt = commands.hsetnx(key, "maxsize", options.maxsize.toString)
+      modFt = commands.hsetnx(key, "modified", time.get(0))
+      exec <- commands.exec()
+      vt <- vtFt
+      delay <- delayFt
+      created <- createdFt
+      mod <- modFt
+      _ <- nsCommands.sadd(s"{$ns}:QUEUES", options.qname)
+    } yield 1L
+  }
+
+  def deleteQueue(qname: String): Future[Unit] = {
+    val key = s"{$ns:$qname}:Q"
+    val qCommands = getRedisCommands(key)
+    val nsCommands = getRedisCommands(s"{$ns}:QUEUES")
+
+    for {
+      _ <- qCommands.del(key)
+      _ <- nsCommands.srem(s"{$ns}:QUEUES")
+    } yield ()
+  }
+
+  def getQueueAttributes(qname: String): Future[RedisQueueAttributes] = {
+    val key = s"{$ns:$qname}"
+    val qCommands = getRedisCommands(key)
+    qCommands.multi()
+    for {
+      time <- qCommands.time()
+      _ = qCommands.multi()
+      qAttFt = qCommands.hmget(
+        s"$key:Q",
+        "vt",
+        "delay",
+        "maxsize",
+        "totalrecv",
+        "totalsent",
+        "created",
+        "modified"
+      )
+      allMsgsFt = qCommands.zcard(key)
+      hiddenMsgsFt = qCommands.zcount(key, time.get(0) + "000", "+inf")
+      _ <- qCommands.exec()
+      qAtt <- qAttFt
+      allMsgs <- allMsgsFt
+      hiddenMsgs <- hiddenMsgsFt
+    } yield RedisQueueAttributes(
+      qname,
+      qAtt.get(0).getValue.toLong,
+      qAtt.get(1).getValue.toLong,
+      qAtt.get(2).getValue.toLong,
+      qAtt.get(3).getValue.toLong,
+      qAtt.get(4).getValue.toLong,
+      OffsetDateTime.ofInstant(
+        Instant.ofEpochMilli(qAtt.get(5).getValue.toLong),
+        ZoneId.systemDefault()
+      ),
+      OffsetDateTime.ofInstant(
+        Instant.ofEpochMilli(qAtt.get(6).getValue.toLong),
+        ZoneId.systemDefault()
+      ),
+      allMsgs,
+      hiddenMsgs
+    )
+  }
+
+  def deleteMessage(id: String, qname: String): Future[Boolean] = {
+    val qKey = s"{$ns:$qname}"
+    val qCommands = getRedisCommands(qname)
+
+    qCommands.multi()
+    val rmFt = qCommands.zrem(qKey, id)
+    val delFt = qCommands.hdel(qKey, id, s"$id:rc", s"$id:fr")
+    for {
+      _ <- qCommands.exec()
+      rm <- rmFt
+      del <- delFt
+    } yield rm == 1 && del > 0
+  }
+
+  private def getQueue(qname: String): Future[RedisQueue] = {
     val queueKey = s"{$ns:$qname}:Q"
     val redis = config.redisClient.fold(
       _.connect().async(),
@@ -51,29 +252,54 @@ class RedisSMQ(
       }
     )
 
-    redis.multi()
+    val txn = redis.multi()
     val queue = redis.hmget(queueKey, "vt", "delay", "maxsize").toScala
     val serverTime = redis.time().toScala
 
-    val q = for {
+    for {
       r <- redis.exec()
       q <- queue
       ts <- serverTime
-    } yield RedisQueue(q.get(0).getValue.toLong, q.get(1)
+      _ = println(ts)
+      _ <- txn
+    } yield RedisQueue(
+      q.get(0).getValue.toLong,
+      q.get(1).getValue().toLong,
+      q.get(2).getValue().toLong,
+      ts.get(0).toLong
+    )
+  }
 
-
-
+  private def changeMessageVisibility(
+      options: ChangeMessageVisibilityOptions
+  ): Future[Long] = {
+    val ChangeMessageVisibilityOptions(qname, id, vt) = options
+    val redis = getRedisCommands(qname)
     for {
-      txn <- redis.multi()
-      _ <- txn.
-    } yield ()
-
+      q <- getQueue(qname)
+      res <- redis.eval[Long](
+        RedisSMQScripts.ChangeMessageVisibility,
+        ScriptOutputType.INTEGER,
+        s"{$ns:$qname}",
+        id,
+        (q.ts + vt * 1000).toString()
+      )
+    } yield res
   }
 
-  private def initScript(script: String): Future[String] = {
-    redis.scriptLoad(script).toScala
-  }
+  // private def initScript(script: String): Future[String] = {
+  //   redis.scriptLoad(script).toScala
+  // }
 
+  private def getRedisCommands(key: String) =
+    config.redisClient.fold(
+      _.connect().async(),
+      cluster => {
+        val node =
+          cluster.getPartitions.getPartitionBySlot(SlotHash.getSlot(key))
+        cluster.connect().getConnection(node.getNodeId).async()
+      }
+    )
 }
 
 object RedisSMQ {
